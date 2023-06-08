6d87fe949fc05e8655310c00d3ff50edee71293b
athulpm <97athulpm@gmail.com>
2023-06-08 12:45:37 +0530

    starting setup

--- dist/app.js
+++ dist/app.js
@@ -3,46 +3,34 @@
 // import fs from 'fs';
 // import { ChatOpenAI } from "langchain/chat_models";
 // import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
-
 // //Load environment variables (populate process.env from .env file)
 // import * as dotenv from "dotenv";
 // dotenv.config();
-
 // const apiKey = process.env.OPENAI_API_KEY;
-
-
 // const chat = new ChatOpenAI({openAIApiKey: apiKey, temperature: 0 });
-
 // export const run = async () => {
 //   const response = await chat.call([
 //     new HumanChatMessage(
 //       "give me a nextjs component that renders an h1 tag with the text Starmorph'"
 //     ),
 //   ]);
-
 //   try {
 //     await fs.promises.writeFile('response.txt', JSON.stringify(response, null, 2));
 //     console.log('File saved successfully.');
 //   } catch (err) {
 //     console.error('Error writing file:', err);
 //   }
-
-
 //   const removeEscapedCharacters = (str: string) => {
 //     return str.replace(/\\n/g, '\n');
 //   };
-  
 //   const processFile = async () => {
 //     try {
 //       // Read the JSON file
 //       const fileContent = await fs.promises.readFile('response.txt', 'utf8');
-  
 //       // Parse the JSON content
 //       const jsonData = JSON.parse(fileContent);
-  
 //       // Extract the 'text' property and remove escaped characters
 //       const text = removeEscapedCharacters(jsonData.text);
-  
 //       // Save the result to a new file
 //       await fs.promises.writeFile('output.txt', text);
 //       console.log('File processed successfully.');
@@ -50,33 +38,25 @@
 //       console.error('Error processing file:', err);
 //     }
 //   };
-  
 //   processFile();
-
-  
 //   console.log(response);
 // };
 // run();
-
 import { ChatOpenAI } from "langchain/chat_models";
-import {HumanChatMessage} from 'langchain/schema'
+import { HumanChatMessage } from 'langchain/schema';
 import * as dotenv from "dotenv";
 dotenv.config();
-
 const openAIApiKey = process.env.OPENAI_API_KEY;
 const chat = new ChatOpenAI({ openAIApiKey });
-
 export const run = async () => {
-  try{
-
-    const response = await chat.call([
-      new HumanChatMessage("Hello, LangChain!"),
-    ]);
-    
-    console.log("Response:", response);
-  } catch(err) {
-    console.log(err)
-  }
+    try {
+        const response = await chat.call([
+            new HumanChatMessage("Hello, LangChain!"),
+        ]);
+        console.log("Response:", response);
+    }
+    catch (err) {
+        console.log(err);
+    }
 };
-
 run();
